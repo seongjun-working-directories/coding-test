79b5887091d594e9f9f0cc3f3a74969189c314fe
seongjun-working-directories <seongjuncode@gmail.com>
2024-10-01 10:41:24 +0000

    코딩테스트 합격자되기 40번, 41번 문제 업로드

--- 코딩테스트_합격자되기/07_Graph/040.py
+++ 코딩테스트_합격자되기/07_Graph/040.py
@@ -1,17 +1,61 @@
 '''
-[문제 040] - 
+[문제 040]
+주어진 그래프와 시작 노드를 이용하여 다익스트라 알고리즘을 구현하는 solution() 함수를 작성하세요.
+인수는 graph, start 총 2개입니다.
+graph는 딕셔너리로 주어지며, 노드의 연결 정보와 가중치가 저장되어 있습니다.
+예를 들어, {'A': {'B': 2, 'C': 5}} 이면, A 는 B, C 에 각각 가중치 2, 5 로 연결되어 있는 것입니다.
+start 는 문자열로 주어지며, 출발 노드를 의미합니다.
+반환값은 시작노드부터 각 노드까지 최소 비용과 최단 경로를 포함한 리스트입니다.
 
 [제약조건]
--
+- 그래프의 노드 개수는 최대 10,000 개 입니다.
+- 각 노드는 0 이상의 정수로 표현합니다.
+- 모든 가중치는 0 이상의 정수이며 10,000 을 넘지 않습니다.
 
 [입출력 예]
-
+graph                           start           return
+{                               'A'             [
+    'A': {'B': 9, 'C': 3},                          {'A': 0, 'B': 4, 'C': 3},
+    'B': {'A': 5},                                  {
+    'C': {'B': 1}                                       'A': ['A'], 'B': ['A', 'C', 'B'], 'C': ['A', 'C']
+}                                                   }
+                                                ]
+{                               'A'             [
+    'A': {'B': 1},                                  {'A': 0, 'B': 4, 'C': 3},
+    'B': {'C': 5},                                  {
+    'C': {'D': 1},                                      'A': ['A'], 'B': ['A', 'B'], 'C': ['A', 'B', 'C'], 'D': ['A', 'B', 'C', 'D']
+    'D': { }                                        }
+}                                               ]
 '''
-def solution_me():
-    pass
+import heapq
 
-def solution():
-    pass
+def solution(graph, start):
+    distances = {node: float("inf") for node in graph}
+    distances[start] = 0
+    queue = []
+    heapq.heappush(queue, [distances[start], start])
+    paths = {start: [start]}
 
-print(solution_me())
-print(solution())
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+
+        if distances[current_node] < current_distance:
+            continue
+
+        for adjacent_node, weight in graph[current_node].items():
+            distance = current_distance + weight
+
+            if distances[adjacent_node] > distance:
+                distances[adjacent_node] = distance
+                paths[adjacent_node] = paths[current_node] + [adjacent_node]
+
+                heapq.heappush(queue, [distance, adjacent_node])
+    
+    sorted_paths = {node: paths[node] for node in sorted(paths)}
+
+    return [distances, sorted_paths]
+
+print(solution(
+    {'A': {'B': 9, 'C': 3}, 'B': {'A': 5}, 'C': {'B': 1}},
+    'A'
+))
--- 코딩테스트_합격자되기/07_Graph/041.py
+++ 코딩테스트_합격자되기/07_Graph/041.py
@@ -1,17 +1,60 @@
 '''
-[문제 041] - 
+[문제 041]
+벨만-포드 알고리즘을 구현한 solution() 함수를 구현하세요.
+graph의 각 데이터는 리스트입니다.
+첫 번째 데이터는 0번 노드 기준으로 연결된 노드 정보, 두 번째 데이터는 1번 노드 기준으로 연결된 노드 정보입니다.
+노드 정보의 구성은 (노드, 가중치)와 같습니다. source는 시작 노드입니다.
+반환값은 최단 거리를 담은 distance 리스트와 최단 거리와 함께 관리할 직전 노드 predecessor를 리스트에 차례로 담아서
+[distance, predecessor] 형식으로 반환하면 됩니다.
+predecessor에서 시작 노드는 None으로 표시합니다.
+만약 음의 가중치 순회가 있다면, [-1]을 반환하세요.
+음의 가중치 순회란 순환하면 할수록 가중치의 합이 적어지는 순회를 말합니다.
 
 [제약조건]
--
+- 노드의 최대 개수는 100 개 입니다.
+- 각 간선의 가중치는 -100 이상 100 이하입니다.
 
 [입출력 예]
-
+graph                               source      return
+[                                   0           [
+    [(1, 4), (2, 3), (4, -6)],                      [0, -2, -4, 3, -6],
+    [(3, 5)],                                       [None, 2, 4, 1, 0]
+    [(1, 2)],                                   ]
+    [(0, 7), (2, 4)],
+    [(2, 2)]
+]
+[                                   0           [-1]
+    [(1, 5), (2, -1)],
+    [(2, 2)],
+    [(3, -2)],
+    [(0, 2), (1, 6)]
+]
 '''
-def solution_me():
-    pass
+def solution(graph, source):
+    # 그래프의 노드 수
+    num_vertices = len(graph)
 
-def solution():
-    pass
+    distance = [float("inf")] * num_vertices
+    distance[source] = 0
 
-print(solution_me())
-print(solution())
+    # 직전 경로 배열 초기화
+    predecessor = [None] * num_vertices
+
+    for tmp in range(num_vertices-1):
+        for u in range(num_vertices):
+            for v, weight in graph[u]:
+                if distance[u] + weight < distance[v]:
+                    distance[v] = distance[u] + weight
+                    predecessor[v] = u
+    
+    for u in range(num_vertices):
+        for v, weight in graph[u]:
+            if distance[u] + weight < distance[v]:
+                return [-1]
+
+    return [distance, predecessor]
+
+print(solution(
+    [[(1, 4), (2, 3), (4, -6)], [(3, 5)], [(1, 2)], [(0, 7), (2, 4)], [(2, 2)]],
+    0
+))
