6a64281f3e2ad3194cf2f60d075a0b6001c9a11c
seongjun-working-directories <seongjuncode@gmail.com>
2024-10-01 05:03:00 +0000

    코딩테스트 합격자되기 38번, 39번 문제 업로드

--- 코딩테스트_합격자되기/07_Graph/038.py
+++ 코딩테스트_합격자되기/07_Graph/038.py
@@ -1,17 +1,50 @@
 '''
-[문제 038] - 
+[문제 038]
+깊이 우선 탐색으로 모든 그래프의 노드를 순회하는 함수 solution()을 작성하세요.
+시작노드는 start로 주어집니다.
+graph는 [출발노드, 도착노드] 쌍들이 들어있는 리스트입니다.
+반환값은 그래프의 시작노드부터 모든 노드를 깊이 우선 탐색으로 진행한 순서대로 노드가 저장된 리스트입니다.
 
 [제약조건]
--
+- 노드의 최대 개수는 100개를 넘지 않습니다.
+- 시작노드부터 시작해서 모든 노드를 방문할 수 있는 경로가 항상 있습니다.
+- 그래프의 노드는 문자열입니다.
 
 [입출력 예]
-
+graph                                                                       start       return
+[['A', 'B'], ['B', 'C'], ['C', 'D'], ['D', 'E']]                            'A'         ['A', 'B', 'C', 'D', 'E']    
+[['A', 'B'], ['A', 'C'], ['B', 'D'], ['B', 'E'], ['C', 'F'], ['E', 'F']]    'A'         ['A', 'B', 'D', 'E', 'F', 'C']
 '''
-def solution_me():
-    pass
+from collections import defaultdict
 
-def solution():
-    pass
+# 1. 입력값을 인접리스트 형태로 변환
+def graph_to_adjacent_list(graph):
+    adjacent_list = defaultdict(list)
+    for u, v in graph:
+        adjacent_list[u].append(v)
+    return adjacent_list
 
-print(solution_me())
-print(solution())
+# 2. DFS 알고리즘을 사용
+def dfs(adjacent_list, node, visited, result):
+    visited.add(node)
+    result.append(node)
+
+    for neighbor in adjacent_list[node]:
+        if neighbor not in visited:
+            dfs(adjacent_list, neighbor, visited, result)
+
+# 3. 실행
+def solution(graph, start):
+    adjacent_list = graph_to_adjacent_list(graph)
+    # print(adjacent_list)
+
+    visited = set()
+    result = []
+    dfs(adjacent_list, start, visited, result)
+    
+    return result
+
+print(solution(
+    [['A', 'B'], ['A', 'C'], ['B', 'D'], ['B', 'E'], ['C', 'F'], ['E', 'F']],
+    'A'
+))
--- 코딩테스트_합격자되기/07_Graph/039.py
+++ 코딩테스트_합격자되기/07_Graph/039.py
@@ -1,17 +1,56 @@
 '''
-[문제 039] - 
+[문제 039]
+너비 우선 탐색으로 모든 노드를 순회하는 함수 solution()을 작성하세요.
+시작노드는 start로 주어집니다.
+graph는 (출발노드, 도착노드) 쌍들이 들어있는 리스트입니다.
+반환값은 그래프의 시작노드부터 모든 노드를 너비 우선 탐색으로 진행한 순서대로 노드가 저장된 리스트입니다.
 
 [제약조건]
--
+- 노드의 최대 개수는 100개를 넘지 않습니다.
+- 시작노드부터 시작해서 모든 노드를 방문할 수 있는 경로가 항상 있습니다.
+- 그래프의 노드는 숫자입니다.
 
 [입출력 예]
-
+graph                                                                               start       return
+[(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (3, 7), (4, 8), (5, 8), (6, 9), (7, 9)]    1           [1, 2, 3, 4, 5, 6, 7, 8, 9]
+[(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 0)]                                    1           [1, 2, 3, 4, 5, 0]
 '''
-def solution_me():
-    pass
+from collections import defaultdict, deque
 
-def solution():
-    pass
+# 1. 입력값을 인접리스트 형태로 변환
+def graph_to_adjacent_list(graph):
+    adjacent_list = defaultdict(list)
+    for u, v in graph:
+        adjacent_list[u].append(v)
+    return adjacent_list
 
-print(solution_me())
-print(solution())
+# 2. BFS 알고리즘을 사용
+def bfs(adjacent_list, start, visited, result):
+    queue = deque([start])
+    visited.add(start)
+    result.append(start)
+
+    while queue:
+        node = queue.popleft()
+        
+        for neighbor in adjacent_list[node]:
+            if neighbor not in visited:
+                queue.append(neighbor)
+                visited.add(neighbor)
+                result.append(neighbor)
+
+# 3. 실행
+def solution(graph, start):
+    adjacent_list = graph_to_adjacent_list(graph)
+    # print(adjacent_list)
+
+    visited = set()
+    result = []
+    bfs(adjacent_list, start, visited, result)
+
+    return result
+
+print(solution(
+    [(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (3, 7), (4, 8), (5, 8), (6, 9), (7, 9)],
+    1
+))
