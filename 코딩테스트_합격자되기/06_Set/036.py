a9b0ae758050e58b2f71c48d242233a6cc46feb1
seongjun-working-directories <seongjuncode@gmail.com>
2024-10-03 07:03:29 +0000

    코딩테스트 합격자되기 36번 문제 업로드

--- 코딩테스트_합격자되기/06_Set/036.py
+++ 코딩테스트_합격자되기/06_Set/036.py
@@ -1,17 +1,32 @@
 '''
-[문제 036] - 
+[문제 036] - https://programmers.co.kr/learn/courses/30/lessons/42577
+전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인하려 합니다.
+전화번호가 다음과 같을 경우, 구조대 전화번호는 영석이의 전화번호의 접두사입니다.
+
+구조대 : 119
+박준영 : 97 674 223
+지영석 : 11 9552 4421
+
+전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때,
+어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
 
 [제약조건]
--
+- phone_book의 길이는 1 이상 1,000,000 이하입니다.
+    - 각 전화번호의 길이는 1 이상 20 이하입니다.
+    - 같은 전화번호가 중복해서 들어있지 않습니다.
 
 [입출력 예]
-
+phone_book	                            return
+["119", "97674223", "1195524421"]	    false
+["123","456","789"]	                    true
+["12","123","1235","567","88"]	        false
 '''
-def solution_me():
-    pass
+def solution(phone_book):
+    sorted_phonebook = sorted(phone_book)
+    
+    for i in range(len(sorted_phonebook)-1):
+        if sorted_phonebook[i+1].startswith(sorted_phonebook[i]):
+            return False
+    return True
 
-def solution():
-    pass
-
-print(solution_me())
-print(solution())
+print(solution(["119", "97674223", "1195524421"]))
